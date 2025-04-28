e8f057b8ce470205bbb9d75d4b169e8719463352
Bhargava Naidu <bhargava@bootlabstech.com>
2025-04-28 16:15:26 +0530

    updated the variable for allow nested items public

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = var.account_replication_type
   allow_nested_items_to_be_public = var.allow_nested_items_tobe_public
   public_network_access_enabled = var.public_network_access_enabled
+  
 }
 
 resource "azurerm_service_plan" "example" {
