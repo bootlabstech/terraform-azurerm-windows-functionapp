a46cf600037a3f7844ec506018d952df27ccf834
Bhargava Naidu <bhargava@bootlabstech.com>
2025-04-30 16:41:54 +0530

    added the app, set and sticky configs

--- main.tf
+++ main.tf
@@ -27,18 +27,60 @@ resource "azurerm_windows_function_app" "example" {
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
   service_plan_id            = azurerm_service_plan.example.id
-  virtual_network_subnet_id = var.virtual_network_subnet_id
+  virtual_network_subnet_id  = var.virtual_network_subnet_id
 
-  site_config {
-    ftps_state             = var.ftps_state
-    app_command_line       = var.app_command_line
-    app_scale_limit        = var.app_scale_limit
-    vnet_route_all_enabled = var.vnet_route_all_enabled
+  builtin_logging_enabled     = var.builtin_logging_enabled
+  daily_memory_time_quota     = var.daily_memory_time_quota
+
+  app_settings = {
+    "AzureBlobStorage:BlobConnectionString"  = var.blob_connection_string
+    "AzureBlobStorage:BlobContainerName"     = var.blob_container_name
+    "AzureWebJobs.QueueTrigger.Disabled"     = var.AzureWebJobs_QueueTrigger_Disabled
+    "FUNCTIONS_INPROC_NET8_ENABLED"          = "1"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT"         = "1"
+    "WEBSITE_USE_PLACEHOLDER_DOTNETISOLATED" = "1"
   }
 
-  depends_on = [ azurerm_service_plan.example ]
+  site_config {
+    ftps_state                           = var.ftps_state
+    app_command_line                     = var.app_command_line
+    app_scale_limit                      = var.app_scale_limit
+    vnet_route_all_enabled               = var.vnet_route_all_enabled
+    runtime_scale_monitoring_enabled     = var.runtime_scale_monitoring_enabled
+    use_32_bit_worker                    = var.use_32_bit_worker
+    application_insights_connection_string = var.app_insights_conn_string != "" ? var.app_insights_conn_string : null
+    application_insights_key               = var.app_insights_key != "" ? var.app_insights_key : null
+
+  }
+
+  connection_string {
+    name  = var.sql_data_connect
+    type  = "SQLAzure"
+    value = var.sql_connection_string
+  }
+
+  sticky_settings {
+    app_setting_names = [
+      "APPINSIGHTS_INSTRUMENTATIONKEY",
+      "AzureBlobStorage:BlobConnectionString",
+      "AzureBlobStorage:BlobContainerName",
+      "AzureWebJobsStorage",
+      "BlobConnectionString",
+      "BlobContainerName",
+    ]
+    connection_string_names = [var.sql_data_connect]
+  }
+
+  tags = {}
+
+  depends_on = [azurerm_service_plan.example]
   
   lifecycle {
-    ignore_changes = all
+    ignore_changes = [
+      app_settings,
+      site_config,
+      sticky_settings,
+      tags
+    ]
   }
 }
--- variables.tf
+++ variables.tf
@@ -28,6 +28,53 @@ variable "public_network_access_enabled" {
   type = bool
   default = false
 }
+variable "AzureWebJobs_QueueTrigger_Disabled" {
+  type = bool
+  default = false  
+}
+variable "builtin_logging_enabled" {
+  type = bool
+  default = false
+}
+variable "daily_memory_time_quota" {
+  type = number
+  default = 2 
+}
+variable "blob_connection_string" {
+  type = string
+}
+variable "blob_container_name" {
+  type = string
+}
+variable "runtime_scale_monitoring_enabled" {
+  type = bool
+  default = true
+}
+variable "app_insights_key" {
+  type = string
+  default = ""
+  sensitive = true
+}
+variable "app_insights_conn_string" {
+  type = string
+  default = ""
+  sensitive = true
+}
+variable "use_32_bit_worker" {
+  type = bool
+  default = false
+
+}
+variable "sql_connection_string" {
+  type = string
+  sensitive = true
+  
+}
+variable "sql_data_connect" {
+  type = string
+  sensitive = true
+
+}
 variable "os_type" {
   type    = string
   default = "Windows"
